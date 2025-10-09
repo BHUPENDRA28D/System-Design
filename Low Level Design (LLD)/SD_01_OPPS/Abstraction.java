fd29d3c272aa64f118cc287fea3fa4c157f7b01b
Bhupendra Makode <76209510+BHUPENDRA28D@users.noreply.github.com>
2025-10-09 10:22:32 +0530

    Repo for System Design

---
+++ Low Level Design (LLD)/SD_01_OPPS/Abstraction.java
@@ -0,0 +1,16 @@
+package SD_01_OPPS;
+
+public class Abstraction {
+}
+
+
+interface Car{
+
+    public  void startEngine();
+    public  void shiftGear();
+    public  void acclerate();
+    public  void brake();
+    public  void stopEngine();
+}
+
+
