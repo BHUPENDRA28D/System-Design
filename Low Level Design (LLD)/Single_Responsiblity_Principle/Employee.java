feecaa6616bfccf4ed03cd4f0375c3b4086373f2
Bhupendra Makode <76209510+BHUPENDRA28D@users.noreply.github.com>
2025-10-23 17:18:52 +0530

    Single Responsiblity Principle

---
+++ Low Level Design (LLD)/Single_Responsiblity_Principle/Employee.java
@@ -0,0 +1,54 @@
+package Single_Responsiblity_Principle;
+/*
+    SRP - Solid Reponsiblity Principle -so this is principal tell us that each class should slove only one or minmum things.
+    >> therfore we created class for varouis methodes.
+
+ */
+public class Employee {
+
+    private final int id;
+
+    public Employee (int id){
+        this.id =id;
+    }
+
+    public  int getId(){
+        return id;
+    }
+/*
+    public int calculateSalary(){
+        return 0;
+    }
+
+    public String fetchBioData(){
+        return "Some Bio Data";
+    }
+
+    public void printPerformanceData(){
+        System.out.println("Printed Performence data here.");
+    }
+    */
+}
+
+//EmpSalary Class
+class EmployeeSalaryCalculator{
+
+    public double calculateSalary(Employee employee){
+        return 0;
+    }
+}
+
+
+//EmpBioData
+class EmpFetchBioData{
+    public String fetchBioData(Employee employee){
+        return "Some Bio Data";
+    }
+}
+
+//EmpPreformanceData
+class EmpPreformanceData{
+    public void printPerformanceData(Employee employee){
+        System.out.println("Printed Performence data here..");
+    }
+}
